03ef54df2bbaaa1d3116779371c34e20de147539
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-09-03 11:32:13 -0500

    Connecting socket.io with server

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:band_names/src/pages/status_page.dart';
+import 'package:band_names/src/services/socket_service.dart';
 import 'package:flutter/material.dart';
 
 // Pages
 import 'package:band_names/src/pages/home_page.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,10 +14,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
         SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        initialRoute: 'home',
-        routes: {'home': (_) => HomePage()});
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => SocketService(),
+        )
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Material App',
+          initialRoute: 'status',
+          routes: {'home': (_) => HomePage(), 'status': (_) => StatusPage()}),
+    );
   }
 }
